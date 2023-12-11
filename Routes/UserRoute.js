774a88a8ecd6cb0b8c735dca646065055941430e
kal-dereje <kalabdereje20@gmail.com>
2023-12-11 22:06:03 +0300

    added profilePicUploader for handler to createUser route

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -9,32 +9,13 @@ const {
 const express = require("express");
 const userRouter = express.Router();
 
-// const FileUploder = require("../MiddleWare/uploders");
-
-// const multer = require("multer");
-
-// const Storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({
-//   storage: Storage,
-// }).single("testImage");
+const { profilePicUploder } = require("../MiddleWare/uploders");
 
 userRouter.route("/getUser").get(GetUser);
-//router.route("/UserCreate").post(upload, UserCreate);
-userRouter.route("/createUser").post(UserCreate);
+userRouter.route("/createUser").post(profilePicUploder, UserCreate);
 userRouter.route("/loginUser").post(LoginUser);
-// router.route("/:id").patch(upload, UpdateUser);
-userRouter
-  .route("/:userId")
-  .get(GetOneUser)
-  .delete(DeleteUser)
-  .patch(UpdateUser);
+// router.route("/:id").patch(profilePicUploder, UpdateUser);
+userRouter.route("/:userId").get(GetOneUser).delete(DeleteUser);
+// .patch(UpdateUser);
 
 module.exports = userRouter;
